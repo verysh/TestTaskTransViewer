b1c46e13a3e57c1ce0cc37d4fdd5902909e99944
verysh <vladerysh@gmail.com>
2025-07-11 11:51:08 +0300

    Initial Commit

---
+++ TestTaskTransViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskTransViewer
+//
+//  Created by Vladimir Eryshev on 11.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
